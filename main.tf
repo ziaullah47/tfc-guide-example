d92a7e66a2614a08c54c04bfef482170c26d11a7
Ziaullah <ziaullah47@gmail.com>
2022-12-05 21:04:55 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Set the Azure Provider source and version being used
-terraform {
-  required_version = ">= 0.14"
+#terraform {
+ # required_version = ">= 0.14"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.1.0"
-    }
-  }
-}
+  #required_providers {
+   # azurerm = {
+    #  source  = "hashicorp/azurerm"
+     # version = "~> 3.1.0"
+    #}
+  #}
+#}
 
 # Configure the Microsoft Azure provider
 provider "azurerm" {
