54eefe4a0a4e6b3ce27f3b1d5280d7fed2f2790a
Ziaullah <ziaullah47@gmail.com>
2022-12-05 21:05:55 +0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -9,6 +9,11 @@ terraform {
       source  = "hashicorp/random"
       version = "3.0.0"
     }
+    
+     azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.1.0"
+    }
   }
 
   required_version = ">= 0.14.0"
