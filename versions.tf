8b611a1ea3f856462139e1723f6dfb3d6f7e0d16
Ziaullah <ziaullah47@gmail.com>
2022-12-08 23:08:12 +0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,20 +1,3 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-    
-     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.1.0"
-    }
-  }
-
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.12"
 }
