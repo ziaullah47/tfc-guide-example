e43fc1233d10b327446ee763aea69815f3847e6e
Ziaullah <ziaullah47@gmail.com>
2022-12-14 10:59:20 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,9 +51,9 @@ resource "azurerm_cosmosdb_account" "acc" {
 }
 
 resource "azurerm_role_assignment" "test" {
-  scope              = "aef2d388-9d56-4a0b-a44b-0e0328922e84"
+  scope              = data.azurerm_client_config.current.object_id
   role_definition_id = "/providers/Microsoft.Authorization/roleDefinitions/36243c78-bf99-498c-9df9-86d9f8d28608"
-  principal_id       = "aef2d388-9d56-4a0b-a44b-0e0328922e84"
+  principal_id       = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_cosmosdb_mongo_database" "mongodb" {
