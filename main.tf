0163298933c6c81b0f52bbfa4efaa1d98b83b9e5
Ziaullah <ziaullah47@gmail.com>
2022-12-15 10:11:40 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,40 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-data "azurerm_client_config" "current" { }
+data "azurerm_subscription" "primary" {
+}
+
+data "azurerm_client_config" "example" {
+}
+
+resource "azurerm_role_definition" "example" {
+  role_definition_id = "00000000-0000-0000-0000-000000000000"
+  name               = "my-custom-role-definition"
+  scope              = data.azurerm_subscription.primary.id
+
+  permissions {
+    actions     = ["Microsoft.Authorization/policyassignments/*", "Microsoft.Authorization/policydefinitions/*"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.primary.id,
+  ]
+}
+
+resource "azurerm_role_assignment" "example" {
+  name               = "00000000-0000-0000-0000-000000000000"
+  scope              = data.azurerm_subscription.primary.id
+  role_definition_id = azurerm_role_definition.example.role_definition_resource_id
+  principal_id       = data.azurerm_client_config.example.object_id
+}
+
+
+
+
+
+
+#data "azurerm_client_config" "current" { }
 
 resource "azurerm_cosmosdb_account" "acc" {
 
@@ -51,17 +84,17 @@ resource "azurerm_cosmosdb_account" "acc" {
   }
 }
 
-data "azurerm_subscription" "primary" {
-}
+#data "azurerm_subscription" "primary" {
+#}
 
-data "azurerm_client_config" "example" {
-}
+#data "azurerm_client_config" "example" {
+#}
 
-resource "azurerm_role_assignment" "test" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Resource Policy Contributor"
-  principal_id         = data.azurerm_client_config.example.object_id
-}
+#resource "azurerm_role_assignment" "test" {
+ # scope                = data.azurerm_subscription.primary.id
+ # role_definition_name = "Resource Policy Contributor"
+ # principal_id         = data.azurerm_client_config.example.object_id
+#}
 
 resource "azurerm_cosmosdb_mongo_database" "mongodb" {
   name                = "cosmosmongodb"
